0d91bb380c1e56fe7b77958861f5804863701851
Logan Godfrey <logalog42@gmail.com>
2022-12-28 12:55:04 -0700

    learning

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
 minetest.register_craftitem("trowel:trowel", {
     description = "My Special Item",
-    inventory_image = "modname_itemname.png"
+    inventory_image = "modname_itemname.png",
+	on_place = function(itemstack, placer, pointed_thing)
+		--local inv = minetest.get_inventory({type="player", name = placer[nametag]})
+        --local lists = inv:get_list('main')
+		--local slot = math.random(1, 8)
+		--minetest.set_node(pointed_thing[above], lists[slot])
+		--local testing = getmetatable(placer)
+		--for i, v in pairs(pointed_thing) do minetest.log('none', tostring(i) .. " = " .. tostring(v)) end
+		minetest.log('none', get_name(placer))
+	end,
+
 })
 
 minetest.register_chatcommand("foo", {
@@ -9,7 +19,9 @@ minetest.register_chatcommand("foo", {
 	},
 	func = function(name, param)
         local inv = minetest.get_inventory({type="player", name = name})
-        local lists = inv:get_lists()
-		for i, v in pairs(lists) do minetest.log('none', tostring(i) .. " = " .. tostring(v)) end      
+        local lists = inv:get_list('main')
+		for i, v in pairs(lists) do minetest.log('none', tostring(i) .. " = " .. tostring(v)) end
+		local slot = math.random(1, 8)
+		minetest.log('none', tostring(lists[slot]))
 	end
 })
